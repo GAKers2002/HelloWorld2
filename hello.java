53802536598a97ef1905b90201a9674a544ded6c
Gabrielle Akers <144271532+GAKers2002@users.noreply.github.com>
2025-02-07 10:56:45 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,9 @@
 public class hello {
 
 	public static void main(String[] args) {
+		System.out.println("Here ye : important announcement");
 		System.out.println("Hello World");
+		System.out.println("Goodbye");
 	}
 
 }
